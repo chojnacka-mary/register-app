f3b04d7d113c8fd09a0b495b2a1d2246121b08b2
Maria Chojnacka <chojnacka.mary@gmail.com>
2018-01-07 20:31:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
