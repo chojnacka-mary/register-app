1f0ecac5343780e3939aba98e582e00651919f49
Maria Chojnacka <chojnacka.mary@gmail.com>
2018-01-08 21:29:46 +0100

    add slick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
-import 'rxjs/add/operator/map'
+import {Component, OnInit} from '@angular/core';
+import 'rxjs/add/operator/map';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RegisterApp';
 
-  constructor() {}
+  ngOnInit() {
+    $('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false
 
-
+    });
+  }
 
 }
